6fab130a8366dc85c9e14f0e3838bdf7ed9de7a7
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-06 18:13:36 +0100

    Update convert_and_humanize.sh
    Added more options

--- convert_and_humanize.sh
+++ convert_and_humanize.sh
@@ -6,29 +6,32 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-# Get the input SID file path from the first argument
 input_sid_path="$1"
-# Extract the filename without the extension to use for output
 base_name=$(basename -- "$input_sid_path")
 file_name="${base_name%.*}"
 
-# Use sidtool to convert the SID file to a temporary MIDI file
-temp_midi_path="/tmp/${file_name}.mid"
+# Get metadata from sidtool
+metadata=$(sidtool --info "$input_sid_path")
+name=$(echo "$metadata" | grep -oP 'Name: \K.*')
+author=$(echo "$metadata" | grep -oP 'Author: \K.*')
+released=$(echo "$metadata" | grep -oP 'Released: \K.*')
+num_songs=$(echo "$metadata" | grep -oP 'Songs: \K\d+')
 
-# Convert .sid to .mid using sidtool
-sidtool --out "$temp_midi_path" --format midi "$input_sid_path"
-
-# Check if sidtool was successful
-if [ $? -eq 0 ]; then
-    # If sidtool succeeded, run the humanize_velocity.py script
-    output_midi_path="${file_name}_Humanized.mid"
-    python3 humanize_velocity.py "$temp_midi_path" "$output_midi_path"
-    
-    # Remove the temporary MIDI file
-    rm "$temp_midi_path"
-    
-    echo "Conversion and humanization complete. Output saved to $output_midi_path"
-else
-    echo "sidtool conversion failed."
+if [ -z "$num_songs" ]; then
+    echo "Could not determine the number of songs in the SID file."
     exit 1
 fi
+
+for ((song=1; song<=num_songs; song++)); do
+    temp_midi_path="/tmp/${file_name}_song${song}.mid"
+    output_midi_path="${file_name}_song${song}_Humanized.mid"
+
+    sidtool --out "$temp_midi_path" --format midi --song $song "$input_sid_path" -f 20000
+    if [ $? -eq 0 ]; then
+        python3 humanize_velocity.py "$temp_midi_path" "$output_midi_path" 0.2 0.01 "$name" "$author" "$released"
+        rm "$temp_midi_path"
+        echo "Conversion and humanization of song $song complete. Output saved to $output_midi_path"
+    else
+        echo "sidtool conversion failed for song $song."
+    fi
+done
